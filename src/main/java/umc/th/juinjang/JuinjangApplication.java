45a596f077376c97cff3dd6e2cd49cc059af53a8
Parkseojin <seojin5565@gmail.com>
2024-01-07 12:29:30 +0900

    [setting] : setting

---
+++ src/main/java/umc/th/juinjang/JuinjangApplication.java
@@ -0,0 +1,13 @@
+package umc.th.juinjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuinjangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuinjangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/th/juinjang/JuinjangApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.th.juinjang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuinjangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
