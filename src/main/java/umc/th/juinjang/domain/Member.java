4efddcab7ab917eb176e85105d14a3d81fd390f8
Parkseojin <seojin5565@gmail.com>
2024-01-16 21:43:20 +0900

    [feat] : Member Entity 완 #3

--- src/main/java/umc/th/juinjang/domain/Member.java
+++ src/main/java/umc/th/juinjang/domain/Member.java
@@ -1,34 +1,45 @@
 package umc.th.juinjang.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import umc.th.juinjang.domain.common.BaseEntity;
+import umc.th.juinjang.domain.enums.Provider;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Member {
+public class Member extends BaseEntity {
 
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long memberId;
-//
-//  private String email;
-//
-//  private String nickname;
-//
-//  Enum
-////  private String provider;
-//
-//  private
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long memberId;
 
+  @Column(nullable = false)
+  private String email;
 
+  @Column(nullable = false)
+  private String nickname;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Provider provider;
+
+  @Column(nullable = false)
+  private String refreshToken;
+
+  @Column(nullable = false)
+  private LocalDateTime refreshTokenExpiresAt;
 }
