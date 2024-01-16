eb7e1bb6c4ad956a98756844e62e05e4b7ef25ce
Parkseojin <seojin5565@gmail.com>
2024-01-17 01:25:39 +0900

    [comment] : enum 주석 추가 #3

--- src/main/java/umc/th/juinjang/domain/enums/ChecklistQuestionCategory.java
+++ src/main/java/umc/th/juinjang/domain/enums/ChecklistQuestionCategory.java
@@ -1,5 +1,6 @@
 package umc.th.juinjang.domain.enums;
 
+
 public enum ChecklistQuestionCategory {
   LOCATION_CONDITION, // 입지여건
   PUBLIC_SPACE, // 공용공간
--- src/main/java/umc/th/juinjang/domain/enums/ChecklistQuestionType.java
+++ src/main/java/umc/th/juinjang/domain/enums/ChecklistQuestionType.java
@@ -1,5 +1,11 @@
 package umc.th.juinjang.domain.enums;
 
 public enum ChecklistQuestionType {
-  SCORE, SELECTED, TEXT
+
+  // 점수식 체크리스트
+  SCORE,
+  // 셀렉트박스 체크리스트
+  SELECTED,
+  // 입력방식 체크리스트
+  TEXT
 }
