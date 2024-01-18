0004f63d2d4551bc35feccc0ce71f5716d1b873b
Parkseojin <seojin5565@gmail.com>
2024-01-19 05:46:08 +0900

    [refactor] : 필요없는 코드 제거 #6

--- src/main/java/umc/th/juinjang/service/LimjangService/LimjangCommandServiceImpl.java
+++ src/main/java/umc/th/juinjang/service/LimjangService/LimjangCommandServiceImpl.java
@@ -86,15 +86,4 @@ public class LimjangCommandServiceImpl implements LimjangCommandService {
     return null;
   }
 
-//  Member newMember = MemberConverter.toMember(request);
-//  List<FoodCategory> foodCategoryList = request.getPreferCategory().stream()
-//      .map(category -> {
-//        return foodCategoryRepository.findById(category).orElseThrow(() -> new FoodCategoryHandler(ErrorStatus.FOOD_CATEGORY_NOT_FOUND));
-//      }).collect(Collectors.toList());
-//
-//  List<MemberPrefer> memberPreferList = MemberPreferConverter.toMemberPreferList(foodCategoryList);
-//
-//        memberPreferList.forEach(memberPrefer -> {memberPrefer.setMember(newMember);});
-//
-//        return memberRepository.save(newMember);
 }
