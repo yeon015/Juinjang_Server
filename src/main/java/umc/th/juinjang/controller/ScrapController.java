a23331fd7cadcce9c5756a70f99146e0450ca87e
Parkseojin <seojin5565@gmail.com>
2024-01-19 08:50:13 +0900

    [add] : ScrapController 추가 #9

--- src/main/java/umc/th/juinjang/controller/ScrapController.java
+++ src/main/java/umc/th/juinjang/controller/ScrapController.java
@@ -1,41 +1,32 @@
 package umc.th.juinjang.controller;
 
-
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.th.juinjang.apiPayload.ApiResponse;
-import umc.th.juinjang.apiPayload.code.status.ErrorStatus;
-import umc.th.juinjang.apiPayload.exception.handler.LimjangHandler;
 import umc.th.juinjang.converter.limjang.LimjangPostConverter;
 import umc.th.juinjang.model.dto.limjang.LimjangPostRequestDTO;
 import umc.th.juinjang.model.dto.limjang.LimjangPostResponseDTO;
-import umc.th.juinjang.service.LimjangService.LimjangCommandService;
+import umc.th.juinjang.model.dto.limjang.LimjangPostResponseDTO.PostDTO;
 
 @RestController
-@RequestMapping("/api/limjang")
+@RequestMapping("/api/scrap")
 @RequiredArgsConstructor
 @Validated
-public class LimjangController {
-
-  private final LimjangCommandService limjangCommandService;
+public class ScrapController {
 
   @CrossOrigin
-  @Operation(summary = "임장 생성 API")
-  @PostMapping("")
-  public ApiResponse<LimjangPostResponseDTO.PostDTO> postLimjang(
-      @RequestBody @Valid LimjangPostRequestDTO.PostDto postDto){
+  @Operation(summary = "스크랩 등록/삭제 API")
+  @PostMapping("/{limjangId}")
+  public ApiResponse<PostDTO> postLimjang(
+      @PathVariable @Valid Long limjangId){
 
-    return ApiResponse.onSuccess(LimjangPostConverter.toPostDTO(limjangCommandService.postLimjang(postDto)));
   }
-
 }
