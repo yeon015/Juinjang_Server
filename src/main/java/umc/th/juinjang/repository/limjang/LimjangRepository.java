48f7824f05f31bbb7ced7f25bd0441fd38919cc1
Parkseojin <seojin5565@gmail.com>
2024-01-19 21:14:10 +0900

    [feat] : 임장 레포에 멤버로 찾는 함수 추가 #11

--- src/main/java/umc/th/juinjang/repository/limjang/LimjangRepository.java
+++ src/main/java/umc/th/juinjang/repository/limjang/LimjangRepository.java
@@ -2,6 +2,9 @@ package umc.th.juinjang.repository.limjang;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.th.juinjang.model.entity.Limjang;
+import umc.th.juinjang.model.entity.LimjangPrice;
 
 public interface LimjangRepository extends JpaRepository<Limjang, Long> {
+
+  Limjang findLimjangByMemberId(Limjang limjangId);
 }
