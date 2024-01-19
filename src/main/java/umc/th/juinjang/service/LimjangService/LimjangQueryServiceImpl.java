2cccb1efaf0bbb89affd580824f94bc4fbf33cc3
Parkseojin <seojin5565@gmail.com>
2024-01-20 01:40:03 +0900

    [feat] : 메인화면 리스트 출력 서비스 #11

--- src/main/java/umc/th/juinjang/service/LimjangService/LimjangQueryServiceImpl.java
+++ src/main/java/umc/th/juinjang/service/LimjangService/LimjangQueryServiceImpl.java
@@ -1,15 +1,21 @@
 package umc.th.juinjang.service.LimjangService;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import umc.th.juinjang.apiPayload.code.status.ErrorStatus;
 import umc.th.juinjang.apiPayload.exception.handler.MemberHandler;
+import umc.th.juinjang.converter.limjang.LimjangMainListConverter;
 import umc.th.juinjang.converter.limjang.LimjangTotalListConverter;
+import umc.th.juinjang.model.dto.limjang.LimjangMainViewListResponsetDTO;
+import umc.th.juinjang.model.dto.limjang.LimjangMainViewListResponsetDTO.RecentUpdatedDto;
 import umc.th.juinjang.model.dto.limjang.LimjangTotalListResponseDTO;
 import umc.th.juinjang.model.dto.limjang.LimjangTotalListResponseDTO.TotalListDto;
+import umc.th.juinjang.model.entity.Image;
 import umc.th.juinjang.model.entity.Limjang;
 import umc.th.juinjang.model.entity.LimjangPrice;
 import umc.th.juinjang.model.entity.Member;
@@ -29,6 +35,7 @@ public class LimjangQueryServiceImpl implements LimjangQueryService{
   private final ScrapRepository scrapRepository;
 
   @Override
+  @Transactional(readOnly = true)
   public LimjangTotalListResponseDTO.TotalListDto getLimjangTotalList() {
 
     // 멤버 찾기(임시구현)
@@ -52,5 +59,23 @@ public class LimjangQueryServiceImpl implements LimjangQueryService{
 
     return LimjangTotalListConverter.toLimjangTotalList(scrapdList, unScrapdList);
 
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<LimjangMainViewListResponsetDTO.ListDto> getLimjangMainList() {
+    // 멤버 찾기(임시구현)
+    Member findMember = memberRepository.findById(1L)
+        .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+    // 멤버가 가지고있는 모든 글
+    List<Limjang> findAllLimjangList = limjangRepository.findLimjangByMemberId(findMember);
+
+    // 멤버가 가지고있는 모든 글
+    return findAllLimjangList.stream()
+        .sorted(Comparator.comparing(Limjang::getUpdatedAt).reversed()) // updatedAt 기준으로 내림차순 정렬
+        .limit(5) // 최대 5개까지만 선택
+        .map(limjang -> LimjangMainListConverter.toLimjangList(limjang, limjang.getPriceId()))
+        .toList();
   }
 }
