0b96d82e5aebc8906043ec49c6f25945b317f387
Parkseojin <seojin5565@gmail.com>
2024-01-19 22:35:02 +0900

    [feat] : 전체 글 조회 Controller 완 #11

--- src/main/java/umc/th/juinjang/controller/LimjangController.java
+++ src/main/java/umc/th/juinjang/controller/LimjangController.java
@@ -9,17 +9,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.th.juinjang.apiPayload.ApiResponse;
-import umc.th.juinjang.apiPayload.code.status.ErrorStatus;
-import umc.th.juinjang.apiPayload.exception.handler.LimjangHandler;
+
 import umc.th.juinjang.converter.limjang.LimjangPostConverter;
 import umc.th.juinjang.model.dto.limjang.LimjangPostRequestDTO;
 import umc.th.juinjang.model.dto.limjang.LimjangPostResponseDTO;
+import umc.th.juinjang.model.dto.limjang.LimjangTotalListResponseDTO;
 import umc.th.juinjang.service.LimjangService.LimjangCommandService;
+import umc.th.juinjang.service.LimjangService.LimjangQueryService;
 
 @RestController
 @RequestMapping("/api/limjang")
@@ -28,14 +31,23 @@ import umc.th.juinjang.service.LimjangService.LimjangCommandService;
 public class LimjangController {
 
   private final LimjangCommandService limjangCommandService;
+  private final LimjangQueryService limjangQueryService;
 
   @CrossOrigin
   @Operation(summary = "임장 생성 API")
   @PostMapping("")
   public ApiResponse<LimjangPostResponseDTO.PostDTO> postLimjang(
       @RequestBody @Valid LimjangPostRequestDTO.PostDto postDto){
-
     return ApiResponse.onSuccess(LimjangPostConverter.toPostDTO(limjangCommandService.postLimjang(postDto)));
+  }
+
+  @CrossOrigin
+  @Operation(summary = "임장 전체 조회 API")
+  @GetMapping("")
+  public ApiResponse<LimjangTotalListResponseDTO.TotalListDto> getLimjangList(
+      ){
+
+    return ApiResponse.onSuccess(limjangQueryService.getLimjangTotalList());
   }
 
 }
--- src/main/java/umc/th/juinjang/repository/limjang/LimjangRepository.java
+++ src/main/java/umc/th/juinjang/repository/limjang/LimjangRepository.java
@@ -1,10 +1,12 @@
 package umc.th.juinjang.repository.limjang;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.th.juinjang.model.entity.Limjang;
 import umc.th.juinjang.model.entity.LimjangPrice;
+import umc.th.juinjang.model.entity.Member;
 
 public interface LimjangRepository extends JpaRepository<Limjang, Long> {
 
-  Limjang findLimjangByMemberId(Limjang limjangId);
+  List<Limjang> findLimjangByMemberId(Member member);
 }
