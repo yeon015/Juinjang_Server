2d02b6ee3ed0281d2e78381b9540850bdfc311ea
Parkseojin <seojin5565@gmail.com>
2024-01-20 01:38:59 +0900

    [feat] : 메인화면 리스트 출력 컨트롤러 #11

--- src/main/java/umc/th/juinjang/controller/LimjangController.java
+++ src/main/java/umc/th/juinjang/controller/LimjangController.java
@@ -3,21 +3,19 @@ package umc.th.juinjang.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.th.juinjang.apiPayload.ApiResponse;
 
+import umc.th.juinjang.converter.limjang.LimjangMainListConverter;
 import umc.th.juinjang.converter.limjang.LimjangPostConverter;
+import umc.th.juinjang.model.dto.limjang.LimjangMainViewListResponsetDTO;
 import umc.th.juinjang.model.dto.limjang.LimjangPostRequestDTO;
 import umc.th.juinjang.model.dto.limjang.LimjangPostResponseDTO;
 import umc.th.juinjang.model.dto.limjang.LimjangTotalListResponseDTO;
@@ -44,10 +42,19 @@ public class LimjangController {
   @CrossOrigin
   @Operation(summary = "임장 전체 조회 API")
   @GetMapping("")
-  public ApiResponse<LimjangTotalListResponseDTO.TotalListDto> getLimjangList(
+  public ApiResponse<LimjangTotalListResponseDTO.TotalListDto> getLimjangTotalList(
       ){
 
     return ApiResponse.onSuccess(limjangQueryService.getLimjangTotalList());
+  }
+
+  @CrossOrigin
+  @Operation(summary = "임장 메인화면에서 최근 임장 조회 API", description = "가장 최근에 수정된 순으로 최대 5개까지 볼 수 있다.")
+  @GetMapping("/main")
+  public ApiResponse<LimjangMainViewListResponsetDTO.RecentUpdatedDto> getLimjangMainList(
+  ){
+
+    return ApiResponse.onSuccess(LimjangMainListConverter.toLimjangMainList(limjangQueryService.getLimjangMainList()));
   }
 
 }
