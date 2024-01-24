b4d9e9d6a822fd9702e46510e910b1b77c49e1d0
2hy2on <mashin0128@gmail.com>
2024-01-24 21:43:41 +0900

    [ãADD] jwt 토큰 생성 발급 & ãìã 유효성 확인

--- src/main/java/umc/th/juinjang/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/umc/th/juinjang/apiPayload/code/status/ErrorStatus.java
@@ -33,8 +33,9 @@ public enum ErrorStatus implements BaseErrorCode {
     CHECKLIST_TYPE_ERROR(HttpStatus.BAD_REQUEST, "CHECKLIST400", "정해지지 않은 요청값입니다. 다시 확인해주세요."),
       
     //JWT 토큰 에러
-    TOKEN_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "TOKEN400", "유효하지 않거나 만료된 토큰입니다.");
-
+    TOKEN_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "TOKEN400", "유효하지 않거나 만료된 토큰입니다."),
+    //JWT 토큰 에러
+    TOKEN_EMPTY(HttpStatus.BAD_REQUEST, "TOKEN401", "토큰값이 존재하지 않습니다.");
     ;
 
     private final HttpStatus httpStatus;
