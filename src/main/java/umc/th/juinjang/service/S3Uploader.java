7df5f49f1d579a375d3d6968f345113c2995a7a5
Parkseojin <seojin5565@gmail.com>
2024-01-28 04:12:47 +0900

    [feat] : s3 업로드,삭제 등 공통 동작하는 파일 추가 #48

---
+++ src/main/java/umc/th/juinjang/service/S3Uploader.java
@@ -0,0 +1,82 @@
+package umc.th.juinjang.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
+@RequiredArgsConstructor    // final 멤버변수가 있으면 생성자 항목에 포함시킴
+@Component
+@Service
+public class S3Uploader {
+
+  private final AmazonS3Client amazonS3Client;
+
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucket;
+
+  // MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
+  public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+    File uploadFile = convert(multipartFile)
+        .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+    return upload(uploadFile, dirName);
+  }
+
+  private String upload(File uploadFile, String dirName) {
+    String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();
+    String uploadImageUrl = putS3(uploadFile, fileName);
+
+    removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
+
+    return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
+  }
+
+  private String putS3(File uploadFile, String fileName) {
+
+    amazonS3Client.putObject(
+        new PutObjectRequest(bucket, fileName, uploadFile)
+//            .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
+    );
+
+    return amazonS3Client.getUrl(bucket, fileName).toString();
+  }
+
+//  //S3에 저장
+//            amazonS3Client.putObject(bucket, "record/"+newfileName, multipartFile.getInputStream(), metadata);
+//  //DB에 저장
+
+  private void removeNewFile(File targetFile) {
+    if(targetFile.delete()) {
+      log.info("파일이 삭제되었습니다.");
+    }else {
+      log.info("파일 삭제 실패");
+    }
+  }
+
+  private Optional<File> convert(MultipartFile file) throws  IOException {
+    String originalFilename = file.getOriginalFilename();
+    String safeFilename = originalFilename.replaceAll("[^a-zA-Z0-9.-]", "_");
+    File convertFile = new File(safeFilename);
+
+    if(convertFile.createNewFile()) {
+      try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+        fos.write(file.getBytes());
+      }
+      return Optional.of(convertFile);
+    }
+    return Optional.empty();
+  }
+
+}
