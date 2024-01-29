8c8247da3f6f2a26aead0ac0f2a4a7cdfaaf9c39
yeon015 <lizkitty@naver.com>
2024-01-29 23:35:16 +0900

    fix: 시큐리티 수정

--- src/main/java/umc/th/juinjang/config/SecurityConfig.java
+++ src/main/java/umc/th/juinjang/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         return web -> web.ignoring()
                 // 로그인 개발 끝나면 삭제 **
                 .requestMatchers("/**", "/swagger-ui/**", "/swagger/**", "/swagger-resources/**", "/swagger-ui.html",
-                        "/configuration/ui",  "/v3/api-docs/**");
+                        "/configuration/ui",  "/v3/api-docs/**", "/h2-console/**");
     }
 
     //선언 방식이 3.x에서 바뀜
