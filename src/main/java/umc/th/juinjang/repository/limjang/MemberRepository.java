b58fae79318697854319e3766c54c5bcffce795b
yeon015 <lizkitty@naver.com>
2024-02-16 00:43:19 +0900

    feat : 닉네임 중복 확인 추가

--- src/main/java/umc/th/juinjang/repository/limjang/MemberRepository.java
+++ src/main/java/umc/th/juinjang/repository/limjang/MemberRepository.java
@@ -11,4 +11,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findByRefreshToken(String refreshToken);
+
+    Member findByNickname(String nickname);
 }
