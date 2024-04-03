b6325f8df2c5e737b6212a4f82d369f3e585f9c5
yeon015 <lizkitty@naver.com>
2024-04-03 23:46:28 +0900

    fix(#107) : 카카오 로그인 로직 수정(프론트에서 사용자 정보  받아오기)

--- src/main/java/umc/th/juinjang/service/auth/OAuthService.java
+++ src/main/java/umc/th/juinjang/service/auth/OAuthService.java
@@ -1,9 +1,6 @@
 package umc.th.juinjang.service.auth;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -12,14 +9,11 @@ import umc.th.juinjang.apiPayload.code.status.ErrorStatus;
 import umc.th.juinjang.apiPayload.exception.handler.MemberHandler;
 import umc.th.juinjang.model.dto.auth.LoginResponseDto;
 import umc.th.juinjang.model.dto.auth.TokenDto;
-import umc.th.juinjang.model.dto.auth.kakao.KakaoOAuthToken;
-import umc.th.juinjang.model.dto.auth.kakao.KakaoUser;
+import umc.th.juinjang.model.dto.auth.kakao.KakaoLoginRequestDto;
 import umc.th.juinjang.model.entity.Member;
 import umc.th.juinjang.model.entity.enums.MemberProvider;
 import umc.th.juinjang.repository.limjang.MemberRepository;
 import umc.th.juinjang.service.JwtService;
-
-import java.io.IOException;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -34,38 +28,11 @@ public class OAuthService {
     private final MemberRepository memberRepository;
     private final JwtService jwtService;
 
-    // 카카오 로그인
-    private final KakaoOauth kakaoOauth;
-    private final HttpServletResponse response;
-
-    // 카카오 로그인
-    // 사용자 로그인 페이지 제공 단계 - url 반환
-    public void request(String socialLoginType) throws IOException {
-        String redirectURL = kakaoOauth.getOauthRedirectURL();
-
-        response.sendRedirect(redirectURL);
-    }
-
-    // code -> accessToken 받아오기
-    // accessToken -> 사용자 정보 받아오기
+    // 카카오 로그인 accessToken, refreshToken 발급
     @Transactional
-    public LoginResponseDto oauthLogin(String socialLoginType, String code) throws JsonProcessingException {
-        // 카카오로 일회성 코드를 보내 액세스 토큰이 담긴 응답객체를 받아옴
-        ResponseEntity<String> accessTokenResponse = kakaoOauth.requestAccessToken(code);
-
-        log.info("response.getBody() = " + accessTokenResponse.getBody());
-
-        // 응답 객체가 JSON 형식으로 되어 있으므로, 이를 deserialization 해서 자바 객체에 담음 (+ 로그 출력됨)
-        KakaoOAuthToken oAuthToken = kakaoOauth.getAccessToken(accessTokenResponse);
-
-        // 액세스 토큰을 다시 카카오로 보내 카카오에 저장된 사용자 정보가 담긴 응답 객체를 받아옴
-        ResponseEntity<String> userInfoResponse = kakaoOauth.requestUserInfo(oAuthToken);
-
-        // 다시 JSON 형식의 응답 객체를 deserialization 해서 자바 객체에 담음
-        KakaoUser kakaoUser = kakaoOauth.getUserInfo(userInfoResponse);
-
-        String email = kakaoUser.getKakao_account().getEmail();
-        log.info(kakaoUser.getKakao_account().getEmail());
+    public LoginResponseDto kakaoLogin(KakaoLoginRequestDto kakaoReqDto) {
+        String email = kakaoReqDto.getEmail();
+        log.info(kakaoReqDto.getEmail());
 
         if(email == null)
             throw new MemberHandler(ErrorStatus.MEMBER_EMAIL_NOT_FOUND);
@@ -81,12 +48,14 @@ public class OAuthService {
                     Member.builder()
                             .email(email)
                             .provider(MemberProvider.KAKAO)
+                            .nickname(kakaoReqDto.getNickname())
                             .refreshToken("")
                             .refreshTokenExpiresAt(LocalDateTime.now())
                             .build()
             );
             System.out.println("member id : " + member.getMemberId());
             System.out.println("member email : " + member.getEmail());
+            System.out.println("member nickname : " + member.getNickname());
         }
 
         // accessToken, refreshToken 발급
@@ -102,7 +71,7 @@ public class OAuthService {
 
         System.out.println("member nickname : " + member.getNickname());
 
-        return new LoginResponseDto(newAccessToken, newRefreshToken, member.getNickname());
+        return new LoginResponseDto(newAccessToken, newRefreshToken, member.getEmail());
     }
 
     // refreshToken으로 accessToken 발급하기
