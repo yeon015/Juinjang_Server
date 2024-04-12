97a2b6f451eeb1f5d37fdf33dcc4caa13ef0562f
Parkseojin <seojin5565@gmail.com>
2024-04-12 18:16:09 +0900

    [refactor] : soft delete hard delete jpql로 구현 #102

--- src/main/java/umc/th/juinjang/repository/limjang/LimjangRepository.java
+++ src/main/java/umc/th/juinjang/repository/limjang/LimjangRepository.java
@@ -1,10 +1,12 @@
 package umc.th.juinjang.repository.limjang;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,8 +15,17 @@ import umc.th.juinjang.model.entity.Member;
 
 @Repository
 public interface LimjangRepository extends JpaRepository<Limjang, Long> {
-  
+
   List<Limjang> findLimjangByMemberId(Member member);
+
+  @Modifying
+  @Query("UPDATE Limjang l SET l.deleted = true, l.updatedAt = CURRENT_TIMESTAMP WHERE l.limjangId = :limjangId")
+  void softDeleteById(@Param("limjangId") Long limjangId);
+
+  // soft delete
+  @Modifying
+  @Query(value ="DELETE FROM limjang l WHERE l.deleted = true AND l.updated_at < :dateTime", nativeQuery = true)
+  void hardDelete(@Param("dateTime") LocalDateTime dateTime);
 
   // 가장 최근에 update된 5개 순서대로
   List<Limjang> findTop5ByMemberIdOrderByUpdatedAtDesc(Member member);
@@ -38,7 +49,6 @@ public interface LimjangRepository extends JpaRepository<Limjang, Long> {
   @Transactional
   @Query("UPDATE Limjang l SET l.memo = :memo WHERE l.limjangId = :limjangId")
   void updateMemo(@Param("limjangId") Long limjangId, @Param("memo") String memo);
-
 
 }
 
