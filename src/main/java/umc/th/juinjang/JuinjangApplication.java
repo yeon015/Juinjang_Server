d0057d6bd9bde52eb06020711ec9c8ab35adf666
mashin2002 <mashin0128@gmail.com>
2024-05-04 22:08:08 +0900

    feignClients bean주입 해결2

--- src/main/java/umc/th/juinjang/JuinjangApplication.java
+++ src/main/java/umc/th/juinjang/JuinjangApplication.java
@@ -1,14 +1,16 @@
 package umc.th.juinjang;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class JuinjangApplication {
 
 	public static void main(String[] args) {
