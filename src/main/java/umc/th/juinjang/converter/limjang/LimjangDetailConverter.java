e5e0a088abeb059ae97a864bc8383727e367db41
Parkseojin <seojin5565@gmail.com>
2024-06-06 19:20:18 +0900

    feat : 임장 상세보기 response converter에  checkListVersion 추가  #157

--- src/main/java/umc/th/juinjang/converter/limjang/LimjangDetailConverter.java
+++ src/main/java/umc/th/juinjang/converter/limjang/LimjangDetailConverter.java
@@ -1,15 +1,20 @@
 package umc.th.juinjang.converter.limjang;
 
 import static umc.th.juinjang.utils.LimjangUtil.makePriceList;
+import static umc.th.juinjang.utils.LimjangUtil.makePriceListVersion2;
 
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 import umc.th.juinjang.model.dto.limjang.LimjangDetailResponseDTO;
 import umc.th.juinjang.model.dto.limjang.LimjangTotalListResponseDTO;
+import umc.th.juinjang.model.dto.limjang.enums.LimjangCheckListVersionEnum;
 import umc.th.juinjang.model.entity.Image;
 import umc.th.juinjang.model.entity.Limjang;
 import umc.th.juinjang.model.entity.LimjangPrice;
+import umc.th.juinjang.model.entity.enums.LimjangPriceType;
+import umc.th.juinjang.model.entity.enums.LimjangPropertyType;
+import umc.th.juinjang.model.entity.enums.LimjangPurpose;
 
 public class LimjangDetailConverter {
 
@@ -22,16 +27,29 @@ public class LimjangDetailConverter {
         .limit(3)
         .toList();
 
-    Integer purposeType = limjang.getPurpose().getValue();
-    Integer priceType = limjang.getPriceType().getValue();
-    List<String> priceList = makePriceList(priceType, purposeType,limjangPrice);
+    LimjangPurpose purposeType = limjang.getPurpose();
+    LimjangPropertyType propertyType = limjang.getPropertyType();
+    LimjangPriceType priceType = limjang.getPriceType();
+
+    LimjangCheckListVersionEnum checkListVersion;
+    if (purposeType == LimjangPurpose.RESIDENTIAL_PURPOSE &&
+        (propertyType == LimjangPropertyType.VILLA||
+            propertyType == LimjangPropertyType.OFFICE_TEL)
+    ){
+      checkListVersion = LimjangCheckListVersionEnum.NON_LIMJANG;
+    } else {
+      checkListVersion = LimjangCheckListVersionEnum.LIMJANG;
+    }
+
+    List<String> priceList = makePriceListVersion2(priceType, purposeType,limjangPrice);
 
     return LimjangDetailResponseDTO.DetailDto.builder()
         .limjangId(limjang.getLimjangId())
+        .checkListVersion(checkListVersion)
         .images(urlList)
         .nickname(limjang.getNickname())
         .purposeCode(limjang.getPurpose().getValue())
-        .priceType(priceType)
+        .priceType(priceType.getValue())
         .priceList(priceList)
         .address(limjang.getAddress())
         .addressDetail(limjang.getAddressDetail())
---
+++ src/main/java/umc/th/juinjang/model/dto/limjang/enums/LimjangCheckListVersionEnum.java
@@ -0,0 +1,6 @@
+package umc.th.juinjang.model.dto.limjang.enums;
+
+public enum LimjangCheckListVersionEnum {
+  LIMJANG,
+  NON_LIMJANG
+}
