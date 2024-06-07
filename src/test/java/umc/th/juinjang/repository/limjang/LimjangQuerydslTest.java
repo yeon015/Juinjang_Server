a4a02123f50336f96088b69d2fdb0d5de56f48de
Parkseojin <seojin5565@gmail.com>
2024-06-07 22:10:26 +0900

    test : Querydsl test 코드 짜봄 #141

--- src/test/java/umc/th/juinjang/repository/limjang/LimjangQuerydslTest.java
+++ src/test/java/umc/th/juinjang/repository/limjang/LimjangQuerydslTest.java
@@ -1,25 +1,32 @@
 package umc.th.juinjang.repository.limjang;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static umc.th.juinjang.repository.limjang.LimjangFixture.FIXTURE_MEMBER;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import umc.th.juinjang.apiPayload.code.status.ErrorStatus;
+import umc.th.juinjang.apiPayload.exception.handler.LimjangHandler;
 import umc.th.juinjang.config.TestConfig;
 import umc.th.juinjang.model.entity.Limjang;
 import umc.th.juinjang.model.entity.Member;
 import umc.th.juinjang.model.entity.enums.LimjangPriceType;
 import umc.th.juinjang.model.entity.enums.LimjangPropertyType;
 import umc.th.juinjang.model.entity.enums.LimjangPurpose;
-import umc.th.juinjang.model.entity.enums.MemberProvider;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @Import(TestConfig.class)
+// @Rollback(value = false)
 public class LimjangQuerydslTest {
 
   @Autowired
@@ -28,44 +35,61 @@ public class LimjangQuerydslTest {
   @Autowired
   private MemberRepository memberRepository;
 
-  @Test
-  @DisplayName("키워드를 전달하면 멤버가 소유한 게시글 중 닉네임, 주소, 상세주소 컬럼중 하나라도 키워드를 포함하는 게시글을 조회할 수 있다.")
-  void searchLimjangsByKeyword() {
+  @Nested
+  class testSerach {
 
-    // given
-    Member member = createMember(1L);
-    memberRepository.save(member);
-    Limjang limjang1 = createLimjang(member, 1L, "경기도 구리시 인창동", "삼성아파트", "우리 집");
-    Limjang limjang2 = createLimjang(member, 2L, "경기도 구리시", "인창", "우리 집");
-    Limjang limjang3 = createLimjang(member, 3L, "경기도 구리시", "어쩌구", "인창");
-    Limjang limjang4 = createLimjang(member, 4L, "어쩌구", "어쩌구", "어쩌구");
-    limjangRepository.saveAll(List.of(limjang1, limjang2, limjang3, limjang4));
+    private Member member;
 
-    // when
-    String keyword = "인창";
-    List<Limjang> findLimjangs = limjangRepository.searchLimjangs(member, keyword);
+    @BeforeEach
+    void setUp() {
+      member = FIXTURE_MEMBER;
+      memberRepository.save(member);
+    }
 
-    // then
-    assertThat(findLimjangs)
-        .extracting(Limjang::getLimjangId)
-        .containsExactlyInAnyOrder(1L, 2L, 3L);
+    @Test
+    @DisplayName("키워드를 전달하면 멤버가 소유한 게시글 중 닉네임, 주소, 상세주소 컬럼중 하나라도 키워드를 포함하는 게시글을 리턴한다.")
+    void testIncludeKeyword() {
+
+      // given
+      Limjang limjang1 = createLimjang(member, "경기도 구리시 인창동", "삼성아파트", "우리 집");
+      Limjang limjang2 = createLimjang(member, "경기도 구리시", "인창", "우리 집");
+      Limjang limjang3 = createLimjang(member, "경기도 구리시", "어쩌구", "인창");
+      limjangRepository.saveAll(List.of(limjang1, limjang2, limjang3));
+
+      // when
+      String keyword = "인창";
+      List<Limjang> findLimjangs = limjangRepository.searchLimjangs(member, keyword);
+      // then
+
+      for (int i = 0; i < findLimjangs.size(); i++) {
+        System.out.println(findLimjangs.get(i).getLimjangId());
+      }
+      assertThat(findLimjangs)
+          .hasSize(3);
+    }
+
+    @Test
+    @DisplayName("주소, 상세주소, 닉네임 중 하나라도 키워드를 포함하지 않는 게시글은 검색에 걸리지 않음")
+    void testNotIncludeKeyword() {
+
+      // given
+      Limjang limjang = createLimjang(member, "어쩌구", "어쩌구", "어쩌구");
+      limjangRepository.save(limjang);
+
+      // when
+      String keyword = "인창";
+      List<Limjang> findLimjangs = limjangRepository.searchLimjangs(member, keyword);
+
+      // then
+      assertThat(findLimjangs)
+          .hasSize(0);
+    }
   }
 
 
-  private Member createMember(Long id) {
-    return Member.builder()
-        .memberId(id)
-        .email("seojin5565@gmail.com")
-        .refreshToken("abcd")
-        .refreshTokenExpiresAt(LocalDateTime.now())
-        .provider(MemberProvider.KAKAO)
-        .build();
-  }
-
-  private Limjang createLimjang(Member member, Long id, String address, String addressDetail, String nickname) {
+  private Limjang createLimjang(Member member, String address, String addressDetail, String nickname) {
     return Limjang.builder()
         .memberId(member)
-        .limjangId(id)
         .purpose(LimjangPurpose.find(0))
         .propertyType(LimjangPropertyType.find(0))
         .priceType(LimjangPriceType.find(3))
