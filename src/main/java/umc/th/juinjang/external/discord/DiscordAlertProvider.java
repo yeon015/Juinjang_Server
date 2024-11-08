f084eb3ff3b963eff4829192ac493aed62c98800
PicturePark1101 <seojin5565@gmail.com>
2024-11-08 13:32:04 +0900

    feat : 디코 알림멘트 변경 #247

--- src/main/java/umc/th/juinjang/external/discord/DiscordAlertProvider.java
+++ src/main/java/umc/th/juinjang/external/discord/DiscordAlertProvider.java
@@ -7,8 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import umc.th.juinjang.apiPayload.code.status.ErrorStatus;
-import umc.th.juinjang.apiPayload.code.status.SuccessStatus;
 import umc.th.juinjang.external.discord.dto.DiscordAlert;
+import umc.th.juinjang.model.entity.Member;
 
 @RequiredArgsConstructor
 @Component
@@ -18,9 +18,9 @@ public class DiscordAlertProvider {
   private final DiscordFeignClient discordFeignClient;
   private final Environment environment;
 
-  public void sendAlert(SuccessStatus successStatus) {
+  public void sendAlert(Member member) {
     if (Arrays.asList(environment.getActiveProfiles()).contains("prod")) {
-      sendAlertToDiscord(DiscordAlert.createAlert(successStatus.getMessage()));
+      sendAlertToDiscord(DiscordAlert.createAlert(member.getMemberId(), member.getProvider(), member.getNickname()));
     }
   }
 
