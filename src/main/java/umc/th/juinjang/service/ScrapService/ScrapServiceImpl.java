6bc2d2075732793ce015ef488ec70448d1c7a138
PicturePark1101 <seojin5565@gmail.com>
2024-12-30 17:07:40 +0900

    refactor : 메소드 분리, 불필요한 주석 제거 #251

--- src/main/java/umc/th/juinjang/service/ScrapService/ScrapServiceImpl.java
+++ src/main/java/umc/th/juinjang/service/ScrapService/ScrapServiceImpl.java
@@ -3,19 +3,16 @@ package umc.th.juinjang.service.ScrapService;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.th.juinjang.apiPayload.code.status.ErrorStatus;
-import umc.th.juinjang.apiPayload.exception.GeneralException;
 import umc.th.juinjang.apiPayload.exception.handler.LimjangHandler;
 import umc.th.juinjang.apiPayload.exception.handler.ScrapHandler;
 import umc.th.juinjang.model.entity.Limjang;
+import umc.th.juinjang.model.entity.Member;
 import umc.th.juinjang.model.entity.Scrap;
-import umc.th.juinjang.model.entity.enums.ScrapActionType;
 import umc.th.juinjang.repository.limjang.LimjangRepository;
 import umc.th.juinjang.repository.limjang.ScrapRepository;
-import umc.th.juinjang.service.LimjangService.LimjangRetriever;
 
 @Slf4j
 @Service
@@ -23,38 +20,40 @@ import umc.th.juinjang.service.LimjangService.LimjangRetriever;
 public class ScrapServiceImpl implements ScrapService {
 
   private final ScrapRepository scrapRepository;
-  private final LimjangRetriever limjangRetriever;
-  private final ScrapRetriever scrapRetriever;
+  private final LimjangRepository limjangRepository;
 
   @Override
   @Transactional
-  public void createScrap(long limjangId) {
-
-    Limjang findLimjang = limjangRetriever.findById(limjangId); // 임장 ID 찾기
-    Optional<Scrap> findScrap = scrapRetriever.findByLimjang(findLimjang);
-
-    if (findScrap.isPresent()) {
-      throw new ScrapHandler(ErrorStatus._SCRAP_ALREADY_SCRAPED);
-    }
-
-    Scrap newScrap = Scrap.builder().limjangId(findLimjang).build();
-    scrapRepository.save(newScrap);
-    System.out.println("--스크랩 완료 in service---");
+  public void createScrap(final Member member, final long limjangId) {
+    Limjang limjang = getLimjangByMemberAndId(member, limjangId);
+    checkScrapAlreadyScraped(limjang);
+    scrapRepository.save(Scrap.create(limjang));
   }
 
   @Override
   @Transactional
-  public void deleteScrap(long limjangId) {
+  public void deleteScrap(final Member member, final long limjangId) {
+    Limjang limjang = getLimjangByMemberAndId(member, limjangId);
+    Scrap scrap = checkScrapAlreadyUnScraped(findByLimjang(limjang));
+    scrapRepository.deleteById(scrap.getScrapId());
+  }
 
-    Limjang findLimjang = limjangRetriever.findById(limjangId); // 임장 ID 찾기
-    Optional<Scrap> findScrap = scrapRetriever.findByLimjang(findLimjang);
+  private Limjang getLimjangByMemberAndId(final Member member, final long limjangId) {
+    return limjangRepository.findLimjangByLimjangIdAndMemberIdAndDeletedIsFalse(limjangId, member)
+        .orElseThrow(() -> new LimjangHandler(ErrorStatus.LIMJANG_NOTFOUND_ERROR));
+  }
 
-    if (findScrap.isEmpty()) {
-      throw new ScrapHandler(ErrorStatus._SCRAP_ALREADY_UNSCRAPED);
-    } else {
-      System.out.println("---------"+findLimjang.getLimjangId()+"------");
-      scrapRepository.deleteByScrapId(findScrap.get().getScrapId());
-      System.out.println("--스크랩 삭제 in service---");
+  private void checkScrapAlreadyScraped(final Limjang limjang) {
+    if (findByLimjang(limjang).isPresent()) {
+      throw new ScrapHandler(ErrorStatus._SCRAP_ALREADY_SCRAPED);
     }
+  }
+
+  private Optional<Scrap> findByLimjang(final Limjang limjang) {
+    return scrapRepository.findByLimjangId(limjang);
+  }
+
+  private Scrap checkScrapAlreadyUnScraped(final Optional<Scrap> scrap) {
+    return scrap.orElseThrow(() -> new ScrapHandler(ErrorStatus._SCRAP_ALREADY_UNSCRAPED));
   }
 }
