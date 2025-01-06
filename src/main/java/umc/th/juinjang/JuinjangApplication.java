c7d687bfdfe64206a3a7e2b4f8ff4e9ca8a17aad
PicturePark1101 <seojin5565@gmail.com>
2025-01-06 21:20:43 +0900

    feat : 비동기 어노테이션 허용 #268

--- src/main/java/umc/th/juinjang/JuinjangApplication.java
+++ src/main/java/umc/th/juinjang/JuinjangApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class JuinjangApplication {
 
